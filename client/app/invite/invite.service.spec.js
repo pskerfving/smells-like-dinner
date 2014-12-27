70ed3e7b40b0fda4881eedb2ba72f5353f73fd4d
Petter Skerfving <pskerfving@me.com>
2014-12-27 16:03:18 +0100

    half assed testing...

--- client/app/invite/invite.service.spec.js
+++ client/app/invite/invite.service.spec.js
@@ -7,13 +7,50 @@ describe('Service: inviteService', function () {
 
   // instantiate service
   var invite;
-  beforeEach(inject(function (_inviteService_) {
+  var auth;
+  var httpBackend;
+
+  beforeEach(inject(function (_inviteService_, _Auth_, $httpBackend) {
     invite = _inviteService_;
+    auth = _Auth_;
+    httpBackend = $httpBackend;
+    httpBackend.whenPOST('/auth/local').respond(200, { token: 'this_is_a_token' } );
+    httpBackend.whenGET('/users/me').respond(200, {
+      name: 'Test User',
+      email: 'test@test.com',
+      role: user,
+      friends_id: {},
+      meals_id: {},
+      schedule_id: 'UNDEFINED',
+      own_schedule_id: 'UNDEFINED'
+    });
+    httpBackend.whenGET('/api/invites/me').respond(200, [
+        {
+          inviter_name: 'Test User 2',
+          inviter_id: 'test_user2_id',
+          invitee_email: 'test@test.com',
+          invitee_id: 'test_user1_id',
+          schedule_id: 'shared_schedule_id',
+          expired: false
+        }
+      ]);
   }));
 
   it('loadInvites() should return a promise', function () {
     var promise = invite.loadInvites();
     expect(promise.hasOwnProperty('then')).toBe(true);
+  });
+
+  it('acceptInvite() should set the schedule_id of the user to the one from the invite', function() {
+    var promise = auth.login({ email: 'test@test.com', password: 'pw' });
+    httpBackend.flush();
+    promise.then(function() {
+      promise = invite.loadInvites();
+      httpBackend.flush();
+      promise.then(function(data) {
+//        expect(data.);
+      })
+    });
   });
 
 });
